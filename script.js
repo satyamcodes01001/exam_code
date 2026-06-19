06ac7c172ada9558d6c14f56a52b96f906f8e86a
Satyam Seth <satyamcodes01001@gmail.com>
2026-06-19 15:03:50 +0530

    Add files via upload
    External_exam

---
+++ script.js
@@ -0,0 +1,11 @@
+function check(){
+
+let n = document.getElementById("num").value;
+
+if(n%2==0)
+    document.getElementById("result").innerHTML="Even Number";
+
+else
+    document.getElementById("result").innerHTML="Odd Number";
+
+}
